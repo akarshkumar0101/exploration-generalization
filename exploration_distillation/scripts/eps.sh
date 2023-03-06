5b97cde4965407ab15c8b79a1499f229b6cfad63
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-03-06 15:34:41 -0500

    episodic reward long training test

---
+++ exploration_distillation/scripts/eps.sh
@@ -0,0 +1,67 @@
+
+python train.py --level 0 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 1 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 2 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 3 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 4 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 5 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 6 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 7 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 8 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 9 --train-obj eps --gamma 0.60 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+
+python train.py --level 0 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 1 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 2 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 3 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 4 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 5 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 6 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 7 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 8 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 9 --train-obj eps --gamma 0.60 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+
+python train.py --level 0 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 1 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 2 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 3 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 4 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 5 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 6 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 7 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 8 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 9 --train-obj eps --gamma 0.60 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+
+
+python train.py --level 0 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 1 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 2 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 3 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 4 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 5 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 6 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 7 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 8 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 9 --train-obj eps --gamma 0.90 --ent-coef 1e-2 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+
+python train.py --level 0 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 1 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 2 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 3 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 4 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 5 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 6 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 7 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 8 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 9 --train-obj eps --gamma 0.90 --ent-coef 1e-1 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+
+python train.py --level 0 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 1 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 2 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 3 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 4 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 5 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 6 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 7 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 8 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
+python train.py --level 9 --train-obj eps --gamma 0.90 --ent-coef 1e-0 --total-timesteps 4e7 --device cuda --track --project test --name {env}_{level:02d}_{train_obj}_{gamma}_{ent_coef}
