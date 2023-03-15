aed1d4d0944a8d230c6a7deb39632ecc37b77f71
Akarsh Kumar <akarshkumar0101@dhcp-10-29-100-233.dyn.MIT.EDU>
2023-03-14 21:28:55 -0400

    unlimited batched dataset distillation

--- exploration_distillation/pretrain.py
+++ exploration_distillation/pretrain.py
@@ -4,17 +4,15 @@ import os
 from distutils.util import strtobool
 from functools import partial
 
-import matplotlib.pyplot as plt
-import models
 import numpy as np
 import torch
 import wandb
 from einops import rearrange
 from tqdm.auto import tqdm
-from train import generate_video
 
-import bc
 import env_utils
+import models
+from train import generate_video
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--device", type=str, default='cpu', help="device to run on")
@@ -24,34 +22,39 @@ parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False,
 parser.add_argument("--project", type=str, default='exploration-distillation')
 parser.add_argument("--name", type=str, default='distill_{env}_{pretrain_levels:05d}_{pretrain_obj}')
 
+parser.add_argument("--load-dir", type=str, default=None)
+
 # Experiment arguments
 parser.add_argument("--env", type=str, default="miner", help="the id of the environment")
 parser.add_argument("--pretrain-levels", type=int, default=1, help='level')
 parser.add_argument("--pretrain-obj", type=str, default='ext', help='objective: ext or int')
 
 parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--coef_entropy', type=float, default=0) # 1e-2
-parser.add_argument('--ds-size', type=float, default=2e9)
-parser.add_argument('--batch-size', type=int, default=512)
-parser.add_argument('--n-steps', type=int, default=50000)
+parser.add_argument('--coef_entropy', type=float, default=0)  # 1e-2
+# parser.add_argument('--ds-size', type=float, default=2e9)
+parser.add_argument('--batch-size', type=int, default=4096)
+parser.add_argument('--n-steps', type=int, default=5000)
+
+parser.add_argument('--n-viz-fast', type=int, default=10000)
+parser.add_argument('--n-viz-slow', type=int, default=10)
 
 # TODO rename pretrain.py to distill.py
 
-def get_level2files(env_name, pretrain_levels, pretrain_obj):
-    levels = list(range(pretrain_levels))
+def get_level2files(args):
+    levels = list(range(args.pretrain_levels))
     level2files = {}
     files = []
     for level in levels:
-        run_name = f"{env_name}_{level:05d}_{pretrain_obj}"
-        run_dir = f'data/{run_name}'
+        run_dir = f'{args.load_dir}/{args.env}_{level:05d}_{args.pretrain_obj}'
         level2files[level] = sorted([f'{run_dir}/{f}' for f in os.listdir(run_dir) if f.startswith('agent')])
         files.append(sorted([f'{run_dir}/{f}' for f in os.listdir(run_dir) if f.startswith('agent')]))
     return level2files
 
+
 def collect_rollout(env, agent, n_steps, device=None):
     x, y, y_probs = [], [], []
     agent = agent.to(device).eval()
-    obs, _ = env.reset() # or just step randomly first time
+    obs, _ = env.reset()  # or just step randomly first time
     for step in range(n_steps):
         obs = torch.from_numpy(obs)
         with torch.no_grad():
@@ -64,15 +67,16 @@ def collect_rollout(env, agent, n_steps, device=None):
         obs, reward, term, trunc, info = env.step(action.tolist())
     return torch.stack(x), torch.stack(y), torch.stack(y_probs)
 
+
 def collect_batch(env_name, level2files, n_agents, n_envs, n_steps, device=None):
     env = env_utils.make_env(1, env_name=f'procgen-{env_name}-v0', level_id=0)
     agent = models.Agent(env)
-    
+
     lf = [(level, file) for level, files in level2files.items() for file in files]
     lf = [lf[i] for i in np.random.choice(len(lf), size=n_agents)]
-    
+
     envs, x, y, y_probs = [], [], [], []
-    for level, file in tqdm(lf):
+    for level, file in lf:
         agent.load_state_dict(torch.load(file))
         env = env_utils.make_env(n_envs, env_name=f'procgen-{env_name}-v0', level_id=level)
         xi, yi, yi_probs = collect_rollout(env, agent, n_steps, device=device)
@@ -82,68 +86,74 @@ def collect_batch(env_name, level2files, n_agents, n_envs, n_steps, device=None)
         y_probs.append(yi_probs)
     return envs, torch.stack(x), torch.stack(y), torch.stack(y_probs)
 
-def eval_performance(agent, env_name, levels, n_steps=2000, device=None):
-    np.random.seed(0)
-    torch.manual_seed(0)
 
+def eval_performance(agent, env_name, levels, n_steps=2000, device=None):
     env = env_utils.make_env(len(levels), env_name=f'procgen-{env_name}-v0', level_id=levels)
     agent = agent.to(device)
-    obs, _ = env.reset() # or just step randomly first time
+    obs, _ = env.reset()  # or just step randomly first time
     for step in tqdm(range(n_steps)):
         obs = torch.from_numpy(obs)
         with torch.no_grad():
             action, _, _, _, _ = agent.get_action_and_value(obs.to(device))
         obs, reward, term, trunc, info = env.step(action.tolist())
-    
     rets_ext = [e.past_returns_ext for e in env.envs]
     rets_eps = [e.past_returns_eps for e in env.envs]
-    rets_ext = np.array([i for sublist in rets_ext for i in sublist])
-    rets_eps = np.array([i for sublist in rets_eps for i in sublist])
     return env, rets_ext, rets_eps
+
 
 def callback(args, main_kwargs, **kwargs):
     data = dict()
-    
-    envs_expert = main_kwargs['envs_expert']
-    envs = [e for envs in envs_expert for e in envs]
-    rets_eps = [r for e in envs for r in e.past_returns_eps]
-    
+
     data['distill/loss_bc'] = kwargs['loss_bc'].item()
     data['distill/loss_entropy'] = kwargs['loss_entropy'].item()
 
+    envs_experts = main_kwargs['batch_data']['envs_expert']
+    envs = [e for svenv in envs_experts for e in svenv.envs]
+    rets_ext = [r for e in envs for r in e.past_returns_ext]
+    rets_eps = [r for e in envs for r in e.past_returns_eps]
+    data['charts_hist/expert_rets_ext_hist'] = wandb.Histogram(rets_ext)
+    data['charts_hist/expert_rets_eps_hist'] = wandb.Histogram(rets_eps)
+    data['charts/expert_rets_ext'] = np.mean(rets_ext)
+    data['charts/expert_rets_eps'] = np.mean(rets_eps)
 
-    if kwargs['i_step']%10==0:
-        main_kwargs['envs_expert']
-    
-    if kwargs['i_step']%(kwargs['n_steps']//10)==0:
-        # seen levels
-        levels = list(range(args.pretrain_levels))
-        env, rets_ext, rets_eps = eval_performance(main_kwargs['agent'], args.env, levels, n_steps=2000, device=args.device)
-        data['charts/seen_rets_ext_hist'] = wandb.Histogram(rets_ext.tolist())
-        data['charts/seen_rets_eps_hist'] = wandb.Histogram(rets_eps.tolist())
-        data['charts/seen_rets_ext'] = np.mean(rets_ext)
-        data['charts/seen_rets_eps'] = np.mean(rets_eps)
-        
-        video = generate_video(env, shape=(2,2))
-        data['media/seen_video'] = wandb.Video(rearrange(video, 't h w c->t c h w'), fps=15)
-        
-        # zero-shot levels
-        levels = list(range(10000, 10000+128))
-        env, rets_ext, rets_eps = eval_performance(main_kwargs['agent'], args.env, levels, n_steps=2000, device=args.device)
-        data['charts/zeroshot_rets_ext_hist'] = wandb.Histogram(rets_ext.tolist())
-        data['charts/zeroshot_rets_eps_hist'] = wandb.Histogram(rets_eps.tolist())
-        data['charts/zeroshot_rets_ext'] = np.mean(rets_ext)
-        data['charts/zeroshot_rets_eps'] = np.mean(rets_eps)
-        
-        video = generate_video(env, shape=(2,2))
-        data['media/zeroshot_video'] = wandb.Video(rearrange(video, 't h w c->t c h w'), fps=15)
-        
+    if kwargs['i_step'] % max(1, (kwargs['n_steps'] // args.n_viz_fast)) == 0:
+        pass
+
+    if kwargs['i_step'] % max(1, (kwargs['n_steps'] // args.n_viz_slow)) == 0:
+        levels_seen = (np.arange(128) % args.pretrain_levels).tolist()
+        levels_zershot = (np.arange(128) + 10000).tolist()
+        levels_all = levels_seen + levels_zershot
+        env, rets_ext, rets_eps = eval_performance(main_kwargs['agent'], args.env, levels_all, n_steps=1000,
+                                                   device=args.device)
+        envs_seen, envs_zeroshot = env.envs[:len(levels_seen)], env.envs[len(levels_seen):]
+        rets_ext_seen, rets_ext_zeroshot = rets_ext[:len(levels_seen)], rets_ext[len(levels_seen):]
+        rets_eps_seen, rets_eps_zeroshot = rets_eps[:len(levels_seen)], rets_eps[len(levels_seen):]
+        # flatten list of list to a list
+        rets_ext_seen = [r for e in rets_ext_seen for r in e]
+        rets_ext_zeroshot = [r for e in rets_ext_zeroshot for r in e]
+        rets_eps_seen = [r for e in rets_eps_seen for r in e]
+        rets_eps_zeroshot = [r for e in rets_eps_zeroshot for r in e]
+
+        data['charts_hist/rets_ext_seen'] = wandb.Histogram(rets_ext_seen)
+        data['charts_hist/rets_eps_seen'] = wandb.Histogram(rets_eps_seen)
+        data['charts_hist/rets_ext_zeroshot'] = wandb.Histogram(rets_ext_zeroshot)
+        data['charts_hist/rets_eps_zeroshot'] = wandb.Histogram(rets_eps_zeroshot)
+        data['charts/rets_ext_seen'] = np.mean(rets_ext_seen)
+        data['charts/rets_eps_seen'] = np.mean(rets_eps_seen)
+        data['charts/rets_ext_zeroshot'] = np.mean(rets_ext_zeroshot)
+        data['charts/rets_eps_zeroshot'] = np.mean(rets_eps_zeroshot)
+
+        video = generate_video(envs_seen, shape=(5, 5))
+        data['media/video_seen'] = wandb.Video(rearrange(video, 't h w c->t c h w'), fps=15)
+        video = generate_video(envs_zeroshot, shape=(5, 5))
+        data['media/video_zeroshot'] = wandb.Video(rearrange(video, 't h w c->t c h w'), fps=15)
+
     if 'pbar' in kwargs:
         kwargs['pbar'].set_postfix({key: val for key, val in data.items() if isinstance(val, (int, float))})
     if args.track:
         wandb.log(data)
-        
-        
+
+
 def train_bc_agent(agent, get_next_batch, batch_size=32, n_steps=10, lr=1e-3, coef_entropy=0.0,
                    device=None, tqdm=None, callback_fn=None):
     """
@@ -153,14 +163,16 @@ def train_bc_agent(agent, get_next_batch, batch_size=32, n_steps=10, lr=1e-3, co
     loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
     opt = torch.optim.Adam(agent.parameters(), lr=lr)
     pbar = range(n_steps)
-    if tqdm is not None: pbar = tqdm(pbar)
+    if tqdm is not None:
+        pbar = tqdm(pbar)
     for i_step in pbar:
         x_batch, y_batch, y_probs_batch = get_next_batch(**locals())
-        x_batch, y_batch, y_probs_batch = x_batch.float().to(device), y_batch.long().to(device), y_probs_batch.float().to(device)
+        x_batch, y_batch, y_probs_batch = x_batch.float().to(device), y_batch.long().to(
+            device), y_probs_batch.float().to(device)
         dist, _, _ = agent.get_dist_and_values(x_batch)
 
-        # loss_bc = loss_fn(dist.logits, y_batch).mean() # class idx loss
-        loss_bc = loss_fn(dist.logits, y_probs_batch).mean() # prob loss
+        # loss_bc = loss_fn(dist.logits, y_batch).mean() # hard action loss
+        loss_bc = loss_fn(dist.logits, y_probs_batch).mean()  # soft action loss
         loss_entropy = dist.entropy().mean()
         loss = loss_bc - coef_entropy * loss_entropy
 
@@ -172,14 +184,14 @@ def train_bc_agent(agent, get_next_batch, batch_size=32, n_steps=10, lr=1e-3, co
             pbar.set_postfix(loss_bc=loss_bc.item(), entropy=loss_entropy.item())
         if callback_fn is not None:
             callback_fn(**locals())
-        
-        
+
+
 def main(args):
     assert args.pretrain_obj in {'ext', 'int', 'eps', 'epd'}
 
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    
+
     print(args)
     # env-level-type-pretrain-levels-type  |  type in {ext, int}
     name = args.name.format(**args.__dict__)
@@ -197,48 +209,53 @@ def main(args):
             # sync_tensorboard=True,
             # monitor_gym=True,
         )
-        
+
     # levels, files = get_levels_files(args.env, args.pretrain_levels, args.pretrain_obj)
     # levels, files, _, n_steps = get_updated_levels_files(levels, files, size=args.ds_size)
-    level2files = get_level2files(args.env, args.pretrain_levels, args.pretrain_obj)
-    level2files = {k: [v[-1]] for k, v in level2files.items()}
+    level2files = get_level2files(args)
+    level2files = {k: [v[-1]] for k, v in level2files.items()}  # only use last file
     # n_steps = int(args.ds_size/(4*64*64*3)/len(level2files))
 
-    # x_train, y_train = collect_dataset(args.env, level2file, n_steps, device=args.device)
-    
     env = env_utils.make_env(1, env_name=f'procgen-{args.env}-v0', level_id=0)
     agent = models.Agent(env)
     n_params = np.sum([p.numel() for p in agent.parameters()])
     print(f'Agent # parameters: {n_params:012d}')
-    
-    cb = partial(callback, args=args, main_kwargs=locals())
-    
-    envs_expert, x_train, y_train, y_probs_train = None, None, None, None
+
+    batch_data = {}
+
     def get_next_batch(i_step, **kwargs):
-        if i_step%10==0:
-            envs_expert, x_train, y_train, y_probs_train = collect_batch(args.env, level2files, 10, 4, 1000, args.device)
-            print(f'Dataset size: {x_train.numel()*x_train.element_size()/1e9} GB')
-            print(f'Dataset: {x_train.shape}, {y_train.shape}, {y_probs_train}, {x_train.dtype}')
+        if i_step % 10 == 0:
+            envs_expert, x_train, y_train, y_probs_train = collect_batch(args.env, level2files, n_agents=10, n_envs=4,
+                                                                         n_steps=1000, device=args.device)
+            print(f'Dataset size: {x_train.numel() * x_train.element_size() / 1e9} GB')
+            print(f'Dataset: {x_train.shape}, {y_train.shape}, {y_probs_train.shape}, {x_train.dtype}')
             x_train = rearrange(x_train, '... fs h w c -> (...) fs h w c')
             y_train = rearrange(y_train, '... -> (...)')
             y_probs_train = rearrange(y_probs_train, '... l -> (...) l')
-            print(f'Dataset: {x_train.shape}, {y_train.shape}, {y_probs_train}, {x_train.dtype}')
-            # log this stuff
-            
+            print(f'Dataset: {x_train.shape}, {y_train.shape}, {y_probs_train.shape}, {x_train.dtype}')
+
+            batch_data['envs_expert'] = envs_expert
+            batch_data['x_train'] = x_train
+            batch_data['y_train'] = y_train
+            batch_data['y_probs_train'] = y_probs_train
+        else:
+            x_train, y_train, y_probs_train = batch_data['x_train'], batch_data['y_train'], batch_data['y_probs_train']
+
         idxs_batch = torch.randperm(len(x_train))[:args.batch_size]
         x_batch, y_batch, y_probs_batch = x_train[idxs_batch], y_train[idxs_batch], y_probs_train[idxs_batch]
         return x_batch, y_batch, y_probs_batch
-    
+
+    cb = partial(callback, args=args, main_kwargs=locals())
     train_bc_agent(agent, get_next_batch, batch_size=args.batch_size,
-                      n_steps=args.n_steps, lr=args.lr, coef_entropy=args.coef_entropy,
-                      device=args.device, tqdm=tqdm, callback_fn=cb)
-    
+                   n_steps=args.n_steps, lr=args.lr, coef_entropy=args.coef_entropy,
+                   device=args.device, tqdm=tqdm, callback_fn=cb)
+
     if args.track:
         os.makedirs(run_dir, exist_ok=True)
         torch.save(agent.state_dict(), f"{run_dir}/agent.pt")
 
     return locals()
 
-if __name__=="__main__":
-    args = parser.parse_args()
-    main(args)
+
+if __name__ == "__main__":
+    main(parser.parse_args())
--- exploration_distillation/train.py
+++ exploration_distillation/train.py
@@ -5,15 +5,15 @@ from distutils.util import strtobool
 from functools import partial
 
 import matplotlib.pyplot as plt
-import models
 import numpy as np
-import ppo_rnd
 import torch
 import wandb
 from einops import rearrange, repeat
 from tqdm.auto import tqdm
 
 import env_utils
+import models
+import ppo_rnd
 
 # TODO
 # plot advantages.std() (the division term)
@@ -28,6 +28,7 @@ parser.add_argument("--project", type=str, default='exploration-distillation')
 parser.add_argument("--name", type=str, default='{env}_{level:05d}_{train_obj}_{pretrain_levels}_{pretrain_obj}')
 
 parser.add_argument("--async-env", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True)
+parser.add_argument("--save-dir", type=str, default=None)
 
 # Experiment arguments
 parser.add_argument("--env", type=str, default="miner", help="the id of the environment")
@@ -38,71 +39,75 @@ parser.add_argument("--pretrain-obj", type=str, default=None, help='objective: e
 
 # Algorithm arguments
 parser.add_argument("--total-timesteps", type=float, default=4e6,
-    help="total timesteps of the experiments")
+                    help="total timesteps of the experiments")
 parser.add_argument("--learning-rate", type=float, default=5e-4,
-    help="the learning rate of the optimizer")
+                    help="the learning rate of the optimizer")
 parser.add_argument("--num-envs", type=int, default=64,
-    help="the number of parallel game environments")
+                    help="the number of parallel game environments")
 parser.add_argument("--num-steps", type=int, default=256,
-    help="the number of steps to run in each environment per policy rollout")
+                    help="the number of steps to run in each environment per policy rollout")
 parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
-    help="Toggle learning rate annealing for policy and value networks")
+                    help="Toggle learning rate annealing for policy and value networks")
 parser.add_argument("--gamma", type=float, default=0.999,
-    help="the discount factor gamma")
+                    help="the discount factor gamma")
 parser.add_argument("--gae-lambda", type=float, default=0.95,
-    help="the lambda for the general advantage estimation")
+                    help="the lambda for the general advantage estimation")
 parser.add_argument("--num-minibatches", type=int, default=32,
-    help="the number of mini-batches")
+                    help="the number of mini-batches")
 parser.add_argument("--update-epochs", type=int, default=1,
-    help="the K epochs to update the policy")
+                    help="the K epochs to update the policy")
 parser.add_argument("--norm-adv", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-    help="Toggles advantages normalization")
+                    help="Toggles advantages normalization")
 parser.add_argument("--clip-coef", type=float, default=0.2,
-    help="the surrogate clipping coefficient")
+                    help="the surrogate clipping coefficient")
 parser.add_argument("--clip-vloss", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
-    help="Toggles whether or not to use a clipped loss for the value function, as per the paper.")
+                    help="Toggles whether or not to use a clipped loss for the value function, as per the paper.")
 parser.add_argument("--ent-coef", type=float, default=0.01,
-    help="coefficient of the entropy")
+                    help="coefficient of the entropy")
 parser.add_argument("--vf-coef", type=float, default=0.5,
-    help="coefficient of the value function")
+                    help="coefficient of the value function")
 parser.add_argument("--max-grad-norm", type=float, default=0.5,
-    help="the maximum norm for the gradient clipping")
+                    help="the maximum norm for the gradient clipping")
 parser.add_argument("--target-kl", type=float, default=None,
-    help="the target KL divergence threshold")
+                    help="the target KL divergence threshold")
 
 # RND arguments
 parser.add_argument("--update-proportion", type=float, default=0.25,
-    help="proportion of exp used for predictor update")
+                    help="proportion of exp used for predictor update")
 parser.add_argument("--int-coef", type=float, default=1.0,
-    help="coefficient of extrinsic reward")
+                    help="coefficient of extrinsic reward")
 parser.add_argument("--ext-coef", type=float, default=2.0,
-    help="coefficient of intrinsic reward")
+                    help="coefficient of intrinsic reward")
 parser.add_argument("--int-gamma", type=float, default=0.99,
-    help="Intrinsic reward discount rate")
+                    help="Intrinsic reward discount rate")
 parser.add_argument("--num-iterations-obs-norm-init", type=int, default=2,
-    help="number of iterations to initialize the observations normalization parameters")
+                    help="number of iterations to initialize the observations normalization parameters")
 
-def generate_video(env, shape=(3, 3)):
+
+def generate_video(envs, shape=(3, 3)):
     """
     Combine X videos and generate a single video of shape (t, h, w, c)
     with appropriate border, looping, padding.
     """
     n_vids = np.prod(shape)
-    assert len(env.envs) >= n_vids
-    ptos = [e.past_traj_obs for e in env.envs[:n_vids]]
+    assert len(envs) >= n_vids
+    ptos = [e.past_traj_obs for e in envs[:n_vids]]
     maxlen = max([len(i) for i in ptos])
+
     def process_video(vid):
-        vid[:, 0, :, :] = 0 # paint black border
-        vid[:, :, 0, :] = 0 # paint black border
+        vid[:, 0, :, :] = 0  # paint black border
+        vid[:, :, 0, :] = 0  # paint black border
         # loop video
-        vid = repeat(vid, 't h w c -> (loop t) h w c', loop=maxlen//len(vid))
+        vid = repeat(vid, 't h w c -> (loop t) h w c', loop=maxlen // len(vid))
         # end padding: repeat last frame
-        padding = repeat(vid[-1], 'h w c -> t h w c', t=maxlen-len(vid))
+        padding = repeat(vid[-1], 'h w c -> t h w c', t=maxlen - len(vid))
         vid = np.concatenate([vid, padding], axis=0)
         return vid
+
     vids = np.stack([process_video(vid) for vid in ptos])
     vids = rearrange(vids, '(b1 b2) t h w c -> t (b1 h) (b2 w) c', b1=shape[0], b2=shape[1])
     return vids
+
 
 def callback(args, main_kwargs, **kwargs):
     env = main_kwargs['env']
@@ -132,12 +137,12 @@ def callback(args, main_kwargs, **kwargs):
     # calc_traj_cov = lambda o:(o.std(axis=0).mean(axis=-1)>0).sum()/first_obs.mean(axis=-1).size
     def pto2heatmap(pto):
         return np.sign(np.abs(np.diff(pto.mean(axis=-1), axis=0))).sum(axis=0)
-    
+
     if np.all([e.past_traj_obs is not None for e in env.envs]):
         heatmaps = np.stack([pto2heatmap(e.past_traj_obs) for e in env.envs])
         heatmap_global = heatmaps.mean(axis=0)
-        covs = [(hm>0).sum()/hm.size for hm in heatmaps]
-        covs_global = (heatmap_global>0).sum()/heatmap_global.size
+        covs = [(hm > 0).sum() / hm.size for hm in heatmaps]
+        covs_global = (heatmap_global > 0).sum() / heatmap_global.size
         data[f'coverage/{1:03d}_trajs'] = np.mean(covs)
         data[f'coverage/{env.num_envs:03d}_trajs'] = covs_global
 
@@ -158,7 +163,7 @@ def callback(args, main_kwargs, **kwargs):
     data['per_step/ext_rewards_hist'] = wandb.Histogram(kwargs['rewards'].flatten().tolist())
     data['per_step/int_rewards_hist'] = wandb.Histogram(kwargs['curiosity_rewards'].flatten().tolist())
 
-    if args.track and (kwargs['update']-5)%(kwargs['num_updates']//20)==0:
+    if args.track and (kwargs['update'] - 5) % (kwargs['num_updates'] // 20) == 0:
         plt.figure(figsize=(10, 7))
         plt.subplot(221)
         plt.imshow(env.envs[0].first_obs)
@@ -168,21 +173,22 @@ def callback(args, main_kwargs, **kwargs):
         plt.tight_layout()
         data['media/heatmap'] = wandb.Image(plt.gcf())
         plt.close('all')
-        
+
     vids_exist = np.all([e.past_traj_obs is not None for e in env.envs])
-    if args.track and vids_exist and (kwargs['update']-5)%(kwargs['num_updates']//20)==0:
-        video = generate_video(env)
+    if args.track and vids_exist and (kwargs['update'] - 5) % (kwargs['num_updates'] // 20) == 0:
+        video = generate_video(env.envs)
         data['media/video'] = wandb.Video(rearrange(video, 't h w c->t c h w'), fps=15)
-    
-    if args.track and kwargs['update']%(kwargs['num_updates']//10)==0:
-        os.makedirs(main_kwargs['run_dir'], exist_ok=True)
-        torch.save(kwargs['agent'].state_dict(), f"{main_kwargs['run_dir']}/agent_{kwargs['update']:05d}.pt")
-        torch.save(kwargs['rnd_model'].state_dict(), f"{main_kwargs['run_dir']}/rnd_{kwargs['update']:05d}.pt")
-        
+
+    if args.save_dir is not None and kwargs['update'] % (kwargs['num_updates'] // 10) == 0:
+        os.makedirs(args.save_dir, exist_ok=True)
+        torch.save(kwargs['agent'].state_dict(), f"{args.save_dir}/agent_{kwargs['update']:05d}.pt")
+        torch.save(kwargs['rnd_model'].state_dict(), f"{args.save_dir}/rnd_{kwargs['update']:05d}.pt")
+
     if 'pbar' in kwargs:
         kwargs['pbar'].set_postfix({key: val for key, val in data.items() if isinstance(val, (int, float))})
     if args.track:
         wandb.log(data)
+
 
 def main(args):
     # assert args.train_obj in {'ext', 'int', 'eps'}
@@ -190,7 +196,7 @@ def main(args):
     args.ext_coef, args.int_coef = {'ext': (1.0, 0.0), 'int': (0.0, 1.0),
                                     'eps': (1.0, 0.0), 'epd': (1.0, 0.0)}[args.train_obj]
     args.total_timesteps = int(args.total_timesteps)
-    
+
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
@@ -199,10 +205,13 @@ def main(args):
     # name = f"{args.env}_{args.level:05d}_{args.train_obj}"
     # if args.pretrain_levels is not None:
     #     name += f"_pretrain_{args.pretrain_levels:05d}_{args.pretrain_obj}"
-    run_dir = f'data/{name}'
-    print(f'project: {args.project}')
-    print(f'name: {name}')
-    print(f'run_dir: {run_dir}')
+
+    if args.save_dir is not None:
+        args.save_dir = f'{args.save_dir}/{args.env}_{args.level:05d}_{args.train_obj}'
+
+    print(f'{args.project=}')
+    print(f'{name=}')
+    print(f'{args.save_dir=}')
 
     if args.track:
         wandb.init(
@@ -214,15 +223,16 @@ def main(args):
             # sync_tensorboard=True,
             # monitor_gym=True,
         )
-        
+
     env = env_utils.make_env(args.num_envs, env_name=f'procgen-{args.env}-v0',
                              level_id=args.level, seed=args.seed, async_=args.async_env, reward_fn=args.train_obj)
     agent = models.Agent(env)
-    
+
     if args.pretrain_levels is not None:
         # load agent from distillation process
-        agent.load_state_dict(torch.load(f"data/distill_{args.env}_{args.pretrain_levels:05d}_{args.pretrain_obj}/agent.pt"))
-    
+        agent.load_state_dict(
+            torch.load(f"data/distill_{args.env}_{args.pretrain_levels:05d}_{args.pretrain_obj}/agent.pt"))
+
     rnd = models.RNDModel(env, (64, 64, 3))
     n_params = np.sum([p.numel() for p in agent.parameters()])
     print(f'Agent # parameters: {n_params:012d}')
@@ -242,7 +252,10 @@ def main(args):
                 )
     return locals()
 
-if __name__=="__main__":
-    args = parser.parse_args()
-    main(args)
 
+def method(a):
+    return a
+
+
+if __name__ == "__main__":
+    main(parser.parse_args())
