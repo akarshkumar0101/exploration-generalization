fba3bb0fd08428aee1a724fd6baaf433cd504343
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-03-21 01:08:19 -0400

    sync

--- exploration_distillation/scripts/2023-03-20.sh
+++ exploration_distillation/scripts/2023-03-20.sh
@@ -5,7 +5,7 @@ python train_multi.py --level-start 0 --n-levels 000100 --train-obj ext --gamma 
 python train_multi.py --level-start 0 --n-levels 001000 --train-obj ext --gamma 0.999 --ent-coef 1e-2 --total-timesteps 50e6 --device cuda --track --project ed-multi --name {env}_{n_levels:08d}_{train_obj}
 python train_multi.py --level-start 0 --n-levels 010000 --train-obj ext --gamma 0.999 --ent-coef 1e-2 --total-timesteps 50e6 --device cuda --track --project ed-multi --name {env}_{n_levels:08d}_{train_obj}
 python train_multi.py --level-start 0 --n-levels 100000 --train-obj ext --gamma 0.999 --ent-coef 1e-2 --total-timesteps 50e6 --device cuda --track --project ed-multi --name {env}_{n_levels:08d}_{train_obj}
-t
+
 python train_multi.py --level-start 0 --n-levels 000001 --train-obj eps --gamma 0.900 --ent-coef 1e-2 --total-timesteps 50e6 --device cuda --track --project ed-multi --name {env}_{n_levels:08d}_{train_obj}
 python train_multi.py --level-start 0 --n-levels 000010 --train-obj eps --gamma 0.900 --ent-coef 1e-2 --total-timesteps 50e6 --device cuda --track --project ed-multi --name {env}_{n_levels:08d}_{train_obj}
 python train_multi.py --level-start 0 --n-levels 000100 --train-obj eps --gamma 0.900 --ent-coef 1e-2 --total-timesteps 50e6 --device cuda --track --project ed-multi --name {env}_{n_levels:08d}_{train_obj}
