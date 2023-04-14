fe5918a09292f50dff40ee8b8aa0e9c3acc3bb1e
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-04-14 13:40:51 -0400

    sync

--- scripts/2023-04-14b.sh
+++ scripts/2023-04-14b.sh
@@ -66,3 +66,4 @@ python test_e3b_idm.py --lr 5e-4 --n-steps 5e3 --batch-size 2048 --idm-merge dif
 python test_e3b_idm.py --lr 5e-4 --n-steps 5e3 --batch-size 2048 --idm-merge both     --idm-normalize False --actions all     --seed 2 --device cuda --track --name e3bidmtest_{idm_merge}_{idm_normalize}_{actions}_{seed} --project e3b_idm_test2
 python test_e3b_idm.py --lr 5e-4 --n-steps 5e3 --batch-size 2048 --idm-merge catdinit --idm-normalize False --actions all     --seed 2 --device cuda --track --name e3bidmtest_{idm_merge}_{idm_normalize}_{actions}_{seed} --project e3b_idm_test2
 
+# python cluster_run.py /data/vision/phillipi/akumar01/exploration-generalization/scripts/2023-04-14b.sh --mem-gpu 7000 --dir /data/vision/phillipi/akumar01/exploration-generalization --servers visiongpu51.csail.mit.edu
