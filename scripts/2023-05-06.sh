d928fd649a78d9b98e9d1f4ca05020dbd1f77bb3
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-05-06 17:08:05 -0400

    make e3b idm model smaller

--- scripts/2023-05-06.sh
+++ scripts/2023-05-06.sh
@@ -53,3 +53,8 @@ python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_
 python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data seq --distribution-mode easy --seed 9 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
 python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data iid --distribution-mode hard --seed 9 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
 python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data seq --distribution-mode hard --seed 9 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
+
+
+
+# python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data iid --distribution-mode easy --seed 0 --device mps
+# python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data seq --distribution-mode easy --seed 0 --device mps
