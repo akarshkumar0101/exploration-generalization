c8f2615d090d74dfb003422b328ac6b2ad5ffb1d
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-05-07 19:03:36 -0400

    test out nov xy miner egb

--- scripts/2023-05-06.sh
+++ scripts/2023-05-06.sh
@@ -14,11 +14,6 @@ python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_
 python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data iid --distribution-mode hard --seed 2 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
 python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data seq --distribution-mode hard --seed 2 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
 
-python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data iid --distribution-mode easy --seed 2 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
-python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data seq --distribution-mode easy --seed 2 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
-python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data iid --distribution-mode hard --seed 2 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
-python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data seq --distribution-mode hard --seed 2 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
-
 python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data iid --distribution-mode easy --seed 3 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
 python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data seq --distribution-mode easy --seed 3 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
 python test_e3b_idm.py --lr 3e-4 --n-steps 2e3 --batch-size 2048 --init kaiming_in --data iid --distribution-mode hard --seed 3 --device cuda --track --project test_e3b_idm_test2 --name run_{data}_{distribution_mode}_{init}_{lr}_{batch_size}_{seed}
