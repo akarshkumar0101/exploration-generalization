d6f0c7520c91e1c04b949f1023ba9c6f7c1c1eaa
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-06-06 21:56:04 -0500

    sync

--- atari/ppo_atari_envpool.py
+++ atari/ppo_atari_envpool.py
@@ -170,6 +170,7 @@ if __name__ == "__main__":
     torch.backends.cudnn.deterministic = args.torch_deterministic
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+    print('using device: ', device)
 
     # env setup
     print('creating env')
