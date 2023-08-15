83d97d4fd1dbd634323cb8d56a88a186887db243
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-08-15 01:27:18 -0400

    goexplore train done

--- atari/goexplore_train.py
+++ atari/goexplore_train.py
@@ -41,13 +41,13 @@ parser.add_argument("--seed", type=int, default=0)
 parser.add_argument("--env_ids", type=str, nargs="+", default=["MontezumaRevenge"])
 parser.add_argument("--strategy", type=str, default="best")
 parser.add_argument("--n_iters", type=lambda x: int(float(x)), default=int(1e4))
-parser.add_argument("--n_envs", type=int, default=1)
-parser.add_argument("--n_steps", type=int, default=128)
-parser.add_argument("--batch_size", type=int, default=256)
-parser.add_argument("--n_updates", type=int, default=16)
+parser.add_argument("--n_envs", type=int, default=8)
+parser.add_argument("--n_steps", type=int, default=512)
+parser.add_argument("--batch_size", type=int, default=384)
+parser.add_argument("--n_updates", type=int, default=32)
 
 parser.add_argument("--ctx_len", type=int, default=32)
-parser.add_argument("--save_agent", type=str, default=None)
+parser.add_argument("--save_agent", type=lambda x: None if x.lower() == "none" else x, default=None)
 
 parser.add_argument("--lr", type=float, default=2.5e-4)
 # parser.add_argument("--lr-warmup", type=lambda x: bool(strtobool(x)), default=True)
@@ -63,6 +63,37 @@ def parse_args(*args, **kwargs):
     return args
 
 
+class MySyncVectorEnv(gym.vector.SyncVectorEnv):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def reset_subenvs(self, ids, seed=None):
+        old_envs, old_obs, old_nenvs = self.envs, self.observations, self.num_envs
+        self.envs = [self.envs[i] for i in ids]
+        self.observations = self.observations[ids]
+        self.num_envs = len(ids)
+        self.reset_async(seed=seed)
+        obs, info = self.reset_wait(seed=seed)
+        self.envs, self.observations, self.num_envs = old_envs, old_obs, old_nenvs
+        return obs, info
+
+
+class MyAsyncVectorEnv(gym.vector.AsyncVectorEnv):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def reset_subenvs(self, ids, seed=None):
+        old_pipes, old_obs, old_nenvs = self.parent_pipes, self.observations, self.num_envs
+        self.parent_pipes = [self.parent_pipes[i] for i in ids]
+        self.observations = self.observations[ids]
+        self.num_envs = len(ids)
+        self.reset_async(seed=seed)
+        obs, info = self.reset_wait(seed=seed)
+        self.parent_pipes, self.observations, self.num_envs = old_pipes, old_obs, old_nenvs
+        # this line long time to debug. shared_memory=False works as expected. if True(default), then obs is a shared memory object
+        return (self.observations[ids], info) if self.shared_memory else (obs, info)
+
+
 class GEBuffer(Buffer):
     def __init__(self, env, n_steps, sample_traj_fn, device=None):
         super().__init__(env, n_steps, device=device)
@@ -74,40 +105,35 @@ class GEBuffer(Buffer):
         self.buf_obs = np.zeros((env.num_envs, n_steps, 84, 84), dtype=np.uint8)
         self.buf_act = np.zeros((env.num_envs, n_steps), dtype=np.uint8)
         self.next_obs = np.zeros_like(env.observation_space.sample())
-        env.envs[0].reset(seed=0)
-        self.ram_start = env.envs[0].ale.getRAM().copy()
+        # env.envs[0].reset(seed=0)
+        # self.ram_start = env.envs[0].ale.getRAM().copy()
 
-    def traj_over_subenv(self, id):
-        self.trajs[id] = self.sample_traj_fn()
-        self.traj_lens[id] = len(self.trajs[id])
-        self.i_locs[id] = 0
-        obs, _ = self.env.envs[id].reset(seed=0)
-        assert np.array_equal(self.env.envs[id].ale.getRAM(), self.ram_start), "env reset to seed=0"
+    def reset_with_newtraj(self, ids):
+        for id in ids:
+            self.trajs[id] = self.sample_traj_fn(id)
+            self.traj_lens[id] = len(self.trajs[id])
+            self.i_locs[id] = 0
+        obs, info = self.env.reset_subenvs(ids, seed=[0 for _ in ids])
+        # assert np.array_equal(self.env.envs[id].ale.getRAM(), self.ram_start), "env reset to seed=0"
         return obs
 
     def gecollect(self, pbar=None):
         if self.first_collect:
             self.first_collect = False
-            # obs, info = self.env.reset()
-            # self.next_obs = info["obs"]
-            # self.sample_new_traj(np.arange(self.env.num_envs))
-            for id in range(self.env.num_envs):
-                self.next_obs[id] = self.traj_over_subenv(id)
-
+            self.next_obs = self.reset_with_newtraj(range(self.env.num_envs))
         for t in range(self.n_steps):
             self.buf_obs[:, t] = self.next_obs
             action = np.array([traj[i_loc] for traj, i_loc in zip(self.trajs, self.i_locs)])
             self.i_locs += 1
             self.buf_act[:, t] = action
             self.next_obs, _, term, trunc, _ = self.env.step(action)
-            if any(term) or any(trunc):
-                print(self.env.envs[0])
-                print(term, trunc)
             assert not any(term) and not any(trunc), "found a done in the ge buffer"
-            # self.dones[:, t] = info["done"]
-            # self.next_obs = info["obs"]
-            for id in np.where(self.i_locs >= self.traj_lens)[0]:
-                self.next_obs[id] = self.traj_over_subenv(id)
+
+            ids_reset = np.where(self.i_locs >= self.traj_lens)[0]
+            if len(ids_reset) > 0:
+                self.next_obs[ids_reset] = self.reset_with_newtraj(ids_reset)
+            if pbar is not None:
+                pbar.update(1)
         self.obss[:, :] = torch.from_numpy(self.buf_obs).to(self.device)
         self.acts[:, :] = torch.from_numpy(self.buf_act).to(self.device)
 
@@ -115,6 +141,7 @@ class GEBuffer(Buffer):
 def make_ge_env_single(env_id):
     env = gym.make(f"ALE/{env_id}-v5", frameskip=1, repeat_action_probability=0.0, full_action_space=True)
     env = gym.wrappers.AtariPreprocessing(env, noop_max=1, frame_skip=4, screen_size=84, grayscale_obs=True)
+    env.getRAM = lambda: env.ale.getRAM()
     return env
 
 
@@ -148,6 +175,9 @@ def get_env_id2trajs(env_id2archives, strategy="best"):
 
 
 def main(args):
+    if args.track:
+        wandb.init(entity=args.entity, project=args.project, name=args.name, config=args, save_code=True)
+
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
@@ -159,30 +189,38 @@ def main(args):
         print(f"env_id: {env_id}, #trajs: {len(trajs)}")
 
     print("Creating buffers")
-    bufs = []
+
+    make_env_fns = []
     for env_id in args.env_ids:
-        env = make_ge_env(env_id, args.n_envs)
+        make_env_fns.extend([partial(make_ge_env_single, env_id=env_id) for _ in range(args.n_envs)])
+    env = MyAsyncVectorEnv(make_env_fns)
 
-        def sample_traj_fn(env_id):
-            trajs = env_id2trajs[env_id]
-            return trajs[np.random.choice(len(trajs))]
+    def sample_traj_fn(id):
+        env_id = args.env_ids[id // args.n_envs]
+        trajs = env_id2trajs[env_id]
+        return trajs[np.random.choice(len(trajs))]
 
-        buf = GEBuffer(env, args.n_steps, sample_traj_fn=partial(sample_traj_fn, env_id=env_id), device=args.device)
-        bufs.append(buf)
-    mbuf = MultiBuffer(bufs)
+    buf = GEBuffer(env, args.n_steps, sample_traj_fn=sample_traj_fn, device=args.device)
 
     print("Creating agent")
     agent = utils.create_agent("gpt", 18, args.ctx_len, load_agent=None, device=args.device)
     opt = agent.create_optimizer(lr=args.lr)
 
     print("Starting learning")
-    for i_iter in tqdm(range(args.n_iters)):
-        for buf in bufs:
-            buf.gecollect()
+    pbar_iters = tqdm(total=args.n_iters)
+    pbar_steps = tqdm(total=args.n_steps)
+    pbar_updates = tqdm(total=args.n_updates)
+    for i_iter in range(args.n_iters):
+        pbar_iters.update(1)
 
-        for i_update in tqdm(range(args.n_updates)):
+        pbar_steps.reset(total=args.n_steps)
+        buf.gecollect(pbar=pbar_steps)
+
+        pbar_updates.reset(total=args.n_updates)
+        for i_update in range(args.n_updates):
+            pbar_updates.update(1)
             assert args.batch_size % (len(args.env_ids) * args.n_envs) == 0
-            batch = mbuf.generate_batch(args.batch_size, args.ctx_len)
+            batch = buf.generate_batch(args.batch_size, args.ctx_len)
             obs, act = batch["obs"], batch["act"]
             obs = obs[:, :, None, :, :]
             logits, values = agent(None, obs, act, None)
@@ -196,6 +234,9 @@ def main(args):
             opt.step()
             # if i % 50 == 0:
             # print(np.e ** loss.item())
+
+            if args.track:
+                wandb.log(dict(loss=loss.item(), ppl=np.e ** loss.item()))
 
 
 # data["trajs"] = np.array([np.array(cell.trajectory, dtype=np.uint8) for cell in archive.values()], dtype=object)
---
+++ atari/scripts_ge/ge_train.py
@@ -0,0 +1,107 @@
+# python cluster_run.py /data/vision/phillipi/akumar01/exploration-generalization/atari/scripts/specialist.sh --mem-gpu 5000 --dir /data/vision/phillipi/akumar01/exploration-generalization/atari --servers freeman-titanxp-1 freeman-titanxp-2 fr    eeman-titanxp-3 freeman-titanxp-5 freeman-titanxp-6 freeman-titanxp-7 torralba-titanxp-1 torralba-titanxp-2 torralba-titanxp-3 torralba-titanxp-4 torralba-titanxp-5 torralba-titanxp-7 --conda-env egb
+# requires 4 GB of GPU memory
+
+python_file = "goexplore_train.py"
+
+import sys
+import os
+import numpy as np
+
+sys.path.append("../")
+import goexplore
+import goexplore_train
+
+default_config = vars(goexplore_train.parser.parse_args())
+config = default_config.copy()
+
+
+def get_arg_list(config):
+    command_list = []
+    for key, val in config.items():
+        # key = key.replace("_", "-")
+        key = f"--{key}"
+
+        if isinstance(val, list):
+            command_list.append(f'{key} {" ".join(val)}')
+        else:
+            if isinstance(val, str) and (" " in val or "=" in val or "[" in val or "]" in val):
+                command_list.append(f'{key}="{val}"')
+            else:
+                command_list.append(f"{key}={val}")
+    return command_list
+
+
+def get_commands(commands):
+    n, l = len(commands), len(commands[0])
+    str_lens = [max([len(command[i]) for command in commands]) for i in range(l)]
+    commands = [" ".join([command[i].ljust(str_lens[i]) for i in range(l)]) for command in commands]
+    return commands
+
+
+# ---------- SHARED CONFIG ---------- #
+config["track"] = False
+config["entity"] = None
+config["project"] = "ge_train"
+config["name"] = None
+# config["log_hist"] = False
+
+config["seed"] = 0
+config["device"] = "cuda"
+
+# config["env_ids"] = ["Asteroids"]
+with open("../atari_games_train_small.txt") as f:
+    config["env_ids"] = [line.strip() for line in f.readlines()]
+config["n_iters"] = int(1e4)
+config["n_envs"] = 8
+config["n_steps"] = 512
+config["batch_size"] = 384
+config["n_updates"] = 32
+
+config["ctx_len"] = 32
+config["save_agent"] = None
+
+config["lr"] = 2.5e-4
+
+config["n_archives"] = 1
+
+shared_config = config.copy()
+
+# ---------- STARTING SWEEPING ---------- #
+configs = []
+sweep_seed = np.arange(0, 1)
+# sweep_env_id = sweep_env_id[:40]
+# with open("../atari_games_test.txt") as f:
+# sweep_env_id += [line.strip() for line in f.readlines()]
+
+np.random.seed(0)
+for seed in sweep_seed:
+    for strategy in ['best', 'random']:
+        config = default_config.copy()
+        config.update(shared_config.copy())
+        config["seed"] = seed
+        config["strategy"] = strategy
+        config["save_agent"] = f"./data/ge_train/generalist_{seed:04d}.pt"
+        configs.append(config.copy())
+
+# assert all configs have same key set
+assert all(c.keys() == configs[0].keys() for c in configs)
+
+for k in list(configs[0].keys()):
+    if all([c[k] == default_config[k] for c in configs]):
+        for c in configs:
+            del c[k]
+
+arg_lists = [[f"python {python_file}"] + get_arg_list(c) for c in configs]
+commands = get_commands(arg_lists)
+# commands = [f"{c} &" for c in commands]
+# commands is a list of strings
+# add a "wait" line every 10 commands
+# wait_every_k = 40
+# commands = [commands[i : i + wait_every_k] for i in range(0, len(commands), wait_every_k)]
+# commands = ["\n".join(c) + "\nwait\n" for c in commands]
+commands = "\n".join(commands)
+
+print(commands)
+out_file = os.path.basename(__file__).replace(".py", ".sh")
+with open(out_file, "w") as f:
+    f.write(commands)
