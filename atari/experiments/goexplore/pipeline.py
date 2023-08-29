0c6b47b376dd5a91026fd7545ae1ed5ecaa8fbeb
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-08-28 23:58:31 -0400

    updated pipeline ftppo

--- atari/experiments/goexplore/pipeline.py
+++ atari/experiments/goexplore/pipeline.py
@@ -122,7 +122,7 @@ print("Done!")
 
 # ------------------------ FINE TUNING ------------------------ #
 
-# python server.py --command_file=~/exploration-generalization/atari/experiments/goexplore/ge_finetune_ppo.sh --run_dir=~/exploration-generalization/atari --experiment_dir=~/experiments/ge_finetune_ppo/ --job_cpu_mem=1000 --job_gpu_mem=4000 --max_jobs_cpu=1 --max_jobs_gpu=3 --max_jobs_node=12 --conda_env=egb
+# python server.py --command_file=~/exploration-generalization/atari/experiments/goexplore/ge_finetune_ppo.sh --run_dir=~/exploration-generalization/atari --experiment_dir=~/experiments/ge_finetune_ppo/ --job_cpu_mem=1000 --job_gpu_mem=4000 --max_jobs_cpu=1 --max_jobs_gpu=3 --max_jobs_node=20 --conda_env=egb
 print("Creating ge_finetune_ppo.sh ...")
 np.random.seed(0)
 default_config = vars(train.parser.parse_args())
