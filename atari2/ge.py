861a744e6b16bdea2e960fc5fc17d349b7c8c671
Akarsh Kumar <akarshkumar0101@gmail.com>
2023-09-07 13:18:16 -0400

    ge and gebc files and requirements

---
+++ atari2/ge.py
@@ -0,0 +1,289 @@
+import argparse
+import os
+from collections import defaultdict
+from distutils.util import strtobool
+
+import cv2
+import gymnasium as gym
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm.auto import tqdm
+import random
+
+import wandb
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False)
+parser.add_argument("--entity", type=str, default=None)
+parser.add_argument("--project", type=str, default="goexplore")
+parser.add_argument("--name", type=str, default=None)
+# parser.add_argument("--log-video", type=lambda x: bool(strtobool(x)), default=False)
+parser.add_argument("--log_hist", type=lambda x: bool(strtobool(x)), default=False)
+
+# parser.add_argument("--device", type=str, default="cpu")
+parser.add_argument("--seed", type=int, default=0)
+
+parser.add_argument("--env_id", type=str, default="MontezumaRevenge")
+parser.add_argument("--n_iters", type=lambda x: int(float(x)), default=int(1e3))
+parser.add_argument("--n_steps", type=int, default=100)
+parser.add_argument("--p_repeat", type=float, default=0.0)
+
+parser.add_argument("--h", type=int, default=8)
+parser.add_argument("--w", type=int, default=11)
+parser.add_argument("--d", type=int, default=8)
+
+parser.add_argument("--max_cells", type=int, default=None)
+parser.add_argument("--use_reward", type=lambda x: bool(strtobool(x)), default=True)
+
+parser.add_argument("--save_archive", type=str, default=None)
+
+
+def parse_args(*args, **kwargs):
+    args = parser.parse_args(*args, **kwargs)
+    for key in ["project", "name", "save_archive"]:
+        if getattr(args, key) is not None:
+            setattr(args, key, getattr(args, key).format(**vars(args)))
+    return args
+
+
+def cellfn(frame, h=8, w=11, d=8):
+    cell = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    cell = cv2.resize(cell, (w, h), interpolation=cv2.INTER_AREA)
+    cell = cell // (256 // d)
+    return cell
+
+
+def hashfn(cell):
+    return hash(cell.tobytes())
+
+
+e1 = 0.001
+e2 = 0.00001
+weight_dict = dict(times_chosen=0.1, times_chosen_since_new=0, times_seen=0.3)
+power_dict = dict(times_chosen=0.5, times_chosen_since_new=0.5, times_seen=0.5)
+
+
+class Cell(object):
+    def __init__(self):
+        self.times_chosen = 0
+        self.times_chosen_since_new = 0
+        self.times_seen = 0
+
+        self.ram = None
+        self.running_ret = 0
+        self.trajectory = []
+
+    def __setattr__(self, key, value):
+        object.__setattr__(self, key, value)
+        if key != "score" and hasattr(self, "times_seen"):
+            self.score = self.cellscore()
+
+    def cntscore(self, a):
+        w, p = weight_dict[a], power_dict[a]
+        v = getattr(self, a)
+        return w / (v + e1) ** p + e2
+
+    def cellscore(self):
+        return self.cntscore("times_chosen") + self.cntscore("times_chosen_since_new") + self.cntscore("times_seen") + 1
+
+    def visit(self):
+        self.times_seen += 1
+        return self.times_seen == 1
+
+    def choose(self):
+        self.times_chosen += 1
+        self.times_chosen_since_new += 1
+        return self.ram, self.running_ret, self.trajectory.copy()
+
+
+def main(args):
+    print(f"Running Goexplore with args: {args}")
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    env = gym.make(f"ALE/{args.env_id}-v5", frameskip=1, repeat_action_probability=0.0, full_action_space=True)
+    assert env.action_space.n == 18
+    env = gym.wrappers.AtariPreprocessing(env, noop_max=1, frame_skip=4, screen_size=210, grayscale_obs=False)
+    _, info = env.reset()
+    max_lives = info["lives"]
+    running_ret = 0
+    action = 0
+    trajectory = []
+
+    max_traj_len = 0
+    max_running_ret = 0
+
+    archive = defaultdict(lambda: Cell())
+
+    if args.track:
+        wandb.init(entity=args.entity, project=args.project, name=args.name, config=args)
+
+    pbar = tqdm(range(args.n_iters))
+    for i_iter in pbar:
+        # ------------------------------- DATA COLLECTION ------------------------------- #
+        found_new_cell = False
+        for i_step in range(args.n_steps):
+            if i_iter == 0 and i_step == 0:
+                action = 0
+            elif not np.random.random() < args.p_repeat:
+                # action = env.action_space.sample()
+                action = np.random.randint(0, env.action_space.n, 1).item()
+            frame, reward, terminal, trunc, info = env.step(action)
+            running_ret += reward
+            terminal |= info["lives"] < max_lives
+            terminal |= trunc
+            trajectory.append(action)
+            max_traj_len = max(max_traj_len, len(trajectory))
+            max_running_ret = max(max_running_ret, running_ret)
+            if terminal:
+                break
+            assert not terminal
+            pixcell = cellfn(frame, h=args.h, w=args.w, d=args.d)
+            cellhash = hashfn(pixcell)
+            cell = archive[cellhash]
+            first_visit = cell.visit()
+
+            replace_cell = first_visit
+            if args.use_reward:
+                replace_cell = replace_cell or running_ret > cell.running_ret or running_ret == cell.running_ret and len(trajectory) < len(cell.trajectory)
+            else:
+                replace_cell = replace_cell or len(trajectory) < len(cell.trajectory)
+
+            if replace_cell:
+                cell.cell_raw = pixcell.flatten().tolist()
+                cell.ram = env.clone_state(True)
+                cell.running_ret = running_ret
+                cell.trajectory = trajectory.copy()
+                cell.times_chosen = 0
+                cell.times_chosen_since_new = 0
+                found_new_cell = True
+        if found_new_cell and i_iter > 0:
+            restore_cell.times_chosen_since_new = 0
+
+        # capping archive size
+        if args.max_cells is not None and (i_iter + 1) % (args.n_iters // 20) == 0 and len(archive) > args.max_cells:
+            hashes = np.array(list(archive.keys()))
+            scores = np.array([cell.score for cell in archive.values()])
+            prune_k = len(archive) - args.max_cells
+            hashes_delete = hashes[np.argsort(scores)[:prune_k]]
+            for cellhash in hashes_delete:
+                del archive[cellhash]
+
+        scores = np.array([cell.score for cell in archive.values()])
+        hashes = [cellhash for cellhash in archive.keys()]
+        probs = scores / scores.sum()
+        restore = np.random.choice(hashes, p=probs)
+        restore_cell = archive[restore]
+        ram, running_ret, trajectory = restore_cell.choose()
+        env.reset()
+        env.restore_state(ram)
+
+        # ------------------------------- LOGGING ------------------------------- #
+        viz_slow = (i_iter + 1) % (args.n_iters // 3) == 0
+        viz_midd = (i_iter + 1) % (args.n_iters // 10) == 0 or viz_slow
+        viz_fast = (i_iter + 1) % (args.n_iters // 100) == 0 or viz_midd
+        data = {}
+        if viz_fast:
+            data["n_cells"] = len(archive)
+            # data["frames"] = 0.0
+            data["max_traj_len"] = max_traj_len
+            data["max_running_ret"] = max_running_ret
+        # if viz_slow:
+        # plt.figure(figsize=(6, 3))
+        # plt.subplot(121)
+        # plt.imshow(frame)
+        # plt.subplot(122)
+        # plt.imshow(pixcell)
+        # plt.tight_layout()
+        # data["cell_repr"] = plt.gcf()
+        # plt.close()
+        if args.track and args.log_hist and viz_slow:
+            traj_lens = np.array([len(cell.trajectory) for cell in archive.values()])
+            traj_rets = np.array([cell.running_ret for cell in archive.values()])
+            data["traj_lens"] = wandb.Histogram(traj_lens)
+            data["traj_rets"] = wandb.Histogram(traj_rets)
+        if args.track and viz_fast:
+            wandb.log(data, step=i_iter)
+        # if viz_midd:
+        # print(f"i_iter: {i_iter: 10d}, n_cells: {len(archive): 10d}, frames: 0, max_running_ret: {max_running_ret: 9.1f}")
+        if viz_fast:
+            pbar.set_postfix(cells=len(archive), ret=max_running_ret)
+
+    # print(np.array([cell.times_seen for cell in archive.values()]))
+    # print(np.array([cell.score for cell in archive.values()]))
+    if args.save_archive is not None:
+        print(f"Saving GE archive with {len(archive)} cells.")
+        save_archive(archive, args, args.save_archive)
+    return archive
+
+
+def save_archive(archive, args, path):
+    data = {}
+    data["traj"] = np.array([np.array(cell.trajectory, dtype=np.uint8) for cell in archive.values()], dtype=object)
+    data["ret"] = np.array([cell.running_ret for cell in archive.values()])
+    data["novelty"] = np.array([cell.score for cell in archive.values()])
+    data["is_leaf"] = filter_substrings(data["traj"])[0]
+    data["config"] = vars(args)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    np.save(path, data)
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.data = None
+
+    def insert_one(self, word, data):
+        node = self
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.data = data
+
+    def insert_all(self, words, datas):
+        for word, data in zip(words, datas):
+            self.insert_one(word, data)
+
+    def trie_num_nodes(root):
+        assert root is not None
+        count = 0
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            count += 1
+            stack.extend(node.children.values())
+        return count
+
+
+def filter_substrings(trajs):
+    root = TrieNode()
+    root.insert_all(trajs, np.arange(len(trajs)))
+
+    ids = []
+    nodes_to_visit = [root]
+    while len(nodes_to_visit) > 0:
+        node = nodes_to_visit.pop(0)
+        nodes_to_visit.extend(node.children.values())
+        if len(node.children) == 0:
+            assert node.data is not None
+            ids.append(node.data)
+    ids = np.array(ids)
+    is_leaf = np.zeros(len(trajs), dtype=bool)
+    is_leaf[ids] = True
+    return is_leaf, ids
+
+
+if __name__ == "__main__":
+    main(parse_args())
+
+
+"""
+# gymnasium
+env = gym.make(f"ALE/MontezumaRevenge-v5", frameskip=1, repeat_action_probability=0.0, full_action_space=True)
+env = gym.wrappers.AtariPreprocessing(env, noop_max=1, frame_skip=4, screen_size=210, grayscale_obs=False)
+
+
+# envpool
+env = envpool.make_gymnasium('MontezumaRevenge-v5', img_height=210, img_width=210, gray_scale=False, stack_num=1, frame_skip=4, repeat_action_probability=0.0, noop_max=1, use_fire_reset=False, full_action_space=True)
+"""
---
+++ atari2/ge_bc.py
@@ -0,0 +1,279 @@
+# Adapted from CleanRL's ppo_atari_envpool.py
+import argparse
+import glob
+import os
+import random
+import time
+from collections import defaultdict
+from distutils.util import strtobool
+from functools import partial
+
+import agent_atari
+import atari_data
+import buffers
+import gymnasium as gym
+import normalize
+import numpy as np
+import timers
+import torch
+import torchinfo
+import utils
+import wandb
+from buffers import Buffer, MultiBuffer
+from einops import rearrange, repeat
+from env_atari import MyEnvpool, ToTensor, make_concat_env
+from torch.distributions import Categorical
+from torch.nn.functional import cross_entropy
+from tqdm.auto import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False)
+parser.add_argument("--entity", type=str, default=None)
+parser.add_argument("--project", type=str, default=None)
+parser.add_argument("--name", type=str, default=None)
+# parser.add_argument("--log-video", type=lambda x: bool(strtobool(x)), default=False)
+# parser.add_argument("--log-hist", type=lambda x: bool(strtobool(x)), default=False)
+
+parser.add_argument("--device", type=str, default="cpu")
+parser.add_argument("--seed", type=int, default=0)
+
+# Algorithm arguments
+parser.add_argument("--env_ids", type=str, nargs="+", default=["MontezumaRevenge"])
+parser.add_argument("--strategy", type=str, default="best")
+parser.add_argument("--n_iters", type=lambda x: int(float(x)), default=int(1e3))
+parser.add_argument("--n_envs", type=int, default=4)
+parser.add_argument("--n_steps", type=int, default=512)
+parser.add_argument("--batch_size", type=int, default=384)
+parser.add_argument("--n_updates", type=int, default=32)
+
+parser.add_argument("--ctx_len", type=int, default=64)
+parser.add_argument("--save_agent", type=lambda x: None if x.lower() == "none" else x, default=None)
+
+parser.add_argument("--lr", type=float, default=1e-4)
+# parser.add_argument("--lr-warmup", type=lambda x: bool(strtobool(x)), default=True)
+# parser.add_argument("--lr-decay", type=str, default="none")
+# parser.add_argument("--max-grad-norm", type=float, default=1.0, help="the maximum norm for the gradient clipping")
+
+parser.add_argument("--ge_data_dir", type=str, default=None)
+parser.add_argument("--n_archives", type=int, default=1)
+
+
+def parse_args(*args, **kwargs):
+    args, uargs = parser.parse_known_args(*args, **kwargs)
+    args.n_envs_per_id = args.n_envs // len(args.env_ids)
+    for k, v in dict([tuple(uarg.replace("--", "").split("=")) for uarg in uargs]).items():
+        setattr(args, k, v)
+    return args
+
+
+class MySyncVectorEnv(gym.vector.SyncVectorEnv):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def reset_subenvs(self, ids, seed=None):
+        old_envs, old_obs, old_nenvs = self.envs, self.observations, self.num_envs
+        self.envs = [self.envs[i] for i in ids]
+        self.observations = self.observations[ids]
+        self.num_envs = len(ids)
+        self.reset_async(seed=seed)
+        obs, info = self.reset_wait(seed=seed)
+        self.envs, self.observations, self.num_envs = old_envs, old_obs, old_nenvs
+        return obs, info
+
+
+class MyAsyncVectorEnv(gym.vector.AsyncVectorEnv):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def reset_subenvs(self, ids, seed=None):
+        old_pipes, old_obs, old_nenvs = self.parent_pipes, self.observations, self.num_envs
+        self.parent_pipes = [self.parent_pipes[i] for i in ids]
+        self.observations = self.observations[ids]
+        self.num_envs = len(ids)
+        self.reset_async(seed=seed)
+        obs, info = self.reset_wait(seed=seed)
+        self.parent_pipes, self.observations, self.num_envs = old_pipes, old_obs, old_nenvs
+        # this line long time to debug. shared_memory=False works as expected. if True(default), then obs is a shared memory object
+        return (self.observations[ids], info) if self.shared_memory else (obs, info)
+
+
+class GEBuffer(Buffer):
+    def __init__(self, env, n_steps, sample_traj_fn, device=None):
+        super().__init__(env, n_steps, device=device)
+        self.trajs = [None for _ in range(self.env.num_envs)]
+        self.traj_lens = np.zeros(self.env.num_envs, dtype=int)
+        self.i_locs = np.zeros(self.env.num_envs, dtype=int)
+        self.sample_traj_fn = sample_traj_fn
+
+        self.buf_obs = np.zeros((env.num_envs, n_steps, 84, 84), dtype=np.uint8)
+        self.buf_act = np.zeros((env.num_envs, n_steps), dtype=np.uint8)
+        self.next_obs = np.zeros_like(env.observation_space.sample())
+        # env.envs[0].reset(seed=0)
+        # self.ram_start = env.envs[0].ale.getRAM().copy()
+
+    def reset_with_newtraj(self, ids):
+        for id in ids:
+            self.trajs[id] = self.sample_traj_fn(id)
+            self.traj_lens[id] = len(self.trajs[id])
+            self.i_locs[id] = 0
+        obs, info = self.env.reset_subenvs(ids, seed=[0 for _ in ids])
+        # assert np.array_equal(self.env.envs[id].ale.getRAM(), self.ram_start), "env reset to seed=0"
+        return obs
+
+    def gecollect(self, pbar=None):
+        if self.first_collect:
+            self.first_collect = False
+            self.next_obs = self.reset_with_newtraj(range(self.env.num_envs))
+        for t in range(self.n_steps):
+            self.buf_obs[:, t] = self.next_obs
+            action = np.array([traj[i_loc] for traj, i_loc in zip(self.trajs, self.i_locs)])
+            self.i_locs += 1
+            self.buf_act[:, t] = action
+            self.next_obs, _, term, trunc, _ = self.env.step(action)
+            assert not any(term) and not any(trunc), "found a done in the ge buffer"
+
+            ids_reset = np.where(self.i_locs >= self.traj_lens)[0]
+            if len(ids_reset) > 0:
+                self.next_obs[ids_reset] = self.reset_with_newtraj(ids_reset)
+            if pbar is not None:
+                pbar.update(1)
+        self.obss[:, :] = torch.from_numpy(self.buf_obs).to(self.device)
+        self.acts[:, :] = torch.from_numpy(self.buf_act).to(self.device)
+
+
+def make_ge_env_single(env_id):
+    env = gym.make(f"ALE/{env_id}-v5", frameskip=1, repeat_action_probability=0.0, full_action_space=True)
+    env = gym.wrappers.AtariPreprocessing(env, noop_max=1, frame_skip=4, screen_size=84, grayscale_obs=True)
+    env.getRAM = lambda: env.ale.getRAM()
+    return env
+
+
+def make_ge_env(env_id, n_envs):
+    make_fn = partial(make_ge_env_single, env_id=env_id)
+    return gym.vector.SyncVectorEnv([make_fn for _ in range(n_envs)])
+
+
+def load_env_id2archives(args):
+    env_id2archives = {}
+    for env_id in tqdm(args.env_ids):
+        files = sorted(glob.glob(f"{args.ge_data_dir}/*{env_id}*"))
+        files = files[: args.n_archives]
+        env_id2archives[env_id] = [np.load(f, allow_pickle=True).item() for f in files]
+    return env_id2archives
+
+
+def get_env_id2trajs(env_id2archives, strategy="best"):
+    env_id2trajs = {}
+    for env_id, archives in tqdm(env_id2archives.items()):
+        env_id2trajs[env_id] = []
+        for archive in archives:
+            trajs, rets, novelty, is_leaf = archive["traj"], archive["ret"], archive["novelty"], archive["is_leaf"]
+            if strategy == "all":
+                idx = np.arange(len(trajs))
+            elif strategy == "best":
+                idx = np.array([np.argmax(rets)])
+            elif strategy == "leaf":
+                idx = np.nonzero(is_leaf)[0]
+            else:
+                raise ValueError(f"Unknown strategy: {strategy}")
+            trajs = trajs[idx]
+            env_id2trajs[env_id].extend(trajs)
+    return env_id2trajs
+
+
+def main(args):
+    if args.track:
+        wandb.init(entity=args.entity, project=args.project, name=args.name, config=args, save_code=True)
+
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    print("Loading archives")
+    env_id2archives = load_env_id2archives(args)
+    print("Creating trajs")
+    env_id2trajs = get_env_id2trajs(env_id2archives, strategy=args.strategy)
+    for env_id, trajs in env_id2trajs.items():
+        print(f"env_id: {env_id}, #trajs: {len(trajs)}")
+
+    print("Creating envs")
+    make_env_fns = []
+    for env_id in args.env_ids:
+        make_env_fns.extend([partial(make_ge_env_single, env_id=env_id) for _ in range(args.n_envs)])
+    env = MyAsyncVectorEnv(make_env_fns)
+    print("Done creating envs!")
+
+    def sample_traj_fn(id):
+        env_id = args.env_ids[id // args.n_envs]
+        trajs = env_id2trajs[env_id]
+        return trajs[np.random.choice(len(trajs))]
+
+    print("Creating buffer")
+    buf = GEBuffer(env, args.n_steps, sample_traj_fn=sample_traj_fn, device=args.device)
+
+    print("Creating agent")
+    agent = utils.create_agent("gpt", 18, args.ctx_len, load_agent=None, device=args.device)
+    opt = agent.create_optimizer(lr=args.lr)
+
+    print("Starting learning")
+    pbar_iters = tqdm(total=args.n_iters)
+    pbar_steps = tqdm(total=args.n_steps)
+    pbar_updates = tqdm(total=args.n_updates)
+    for i_iter in range(args.n_iters):
+        pbar_iters.update(1)
+
+        pbar_steps.reset(total=args.n_steps)
+        buf.gecollect(pbar=pbar_steps)
+
+        pbar_updates.reset(total=args.n_updates)
+        for i_update in range(args.n_updates):
+            pbar_updates.update(1)
+            assert args.batch_size % (len(args.env_ids) * args.n_envs) == 0
+            batch = buf.generate_batch(args.batch_size, args.ctx_len)
+            obs, act = batch["obs"], batch["act"]
+            obs = obs[:, :, None, :, :]
+            logits, values = agent(None, obs, act, None)
+
+            loss_bc = torch.nn.functional.cross_entropy(rearrange(logits, "b t d -> (b t) d"), rearrange(act, "b t -> (b t)"), reduction="none")
+            loss_bc = rearrange(loss_bc, "(b t) -> b t", b=args.batch_size)
+            loss = loss_bc.mean()
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+            # if i % 50 == 0:
+            # print(np.e ** loss.item())
+
+            if args.track:
+                data = {}
+                data["loss"] = loss.item()
+                data["ppl"] = np.e ** loss.item()
+
+                if i_iter % (args.n_iters // 100) == 0 and i_update == 0:
+                    ppl = loss_bc.mean(dim=0).exp().detach().cpu().numpy()
+                    pos = np.arange(len(ppl))
+                    table = wandb.Table(data=np.stack([pos, ppl], axis=-1), columns=["ctx_pos", "ppl"])
+                    data["ppl_vs_ctx_pos"] = wandb.plot.line(table, "ctx_pos", "ppl", title="PPL vs Context Position")
+                wandb.log(data)
+
+        if args.save_agent is not None and i_iter % (args.n_iters // 100) == 0:
+            save_agent = args.save_agent.format(**locals())
+            os.makedirs(os.path.dirname(save_agent), exist_ok=True)
+            torch.save(agent.state_dict(), save_agent)
+
+
+# data["trajs"] = np.array([np.array(cell.trajectory, dtype=np.uint8) for cell in archive.values()], dtype=object)
+# data["rets"] = np.array([cell.running_ret for cell in archive.values()])
+# data["scores"] = np.array([cell.score for cell in archive.values()])
+
+
+if __name__ == "__main__":
+    main(parse_args())
+
+# TODO do goexplore runs with different seeds
+
+
+# plt.plot(pos, ppl)
+# plt.ylim(0, args.vocab_size)
+# plt.ylabel("PPL")
+# plt.xlabel("Token Position")
+# data["mpl"] = plt
